c123856475da3da16b18887a15fbe34c5ae24c4d
anastshak <nastyashakura@gmail.com>
2024-09-17 01:25:38 +0300

    feat: add gallery of random artworks

--- src/components/MainHome/Main.tsx
+++ src/components/MainHome/Main.tsx
@@ -1,4 +1,6 @@
 import { Gallery } from '@/components/Gallery/Gallery';
+import { RandomGallery } from '@/components/RandomGallery/RandomGallery';
+
 import styles from './Main.module.scss';
 
 export const Main = () => {
@@ -8,6 +10,7 @@ export const Main = () => {
         let's find some <span>art</span> here!
       </h1>
       <Gallery />
+      <RandomGallery />
     </main>
   );
 };
