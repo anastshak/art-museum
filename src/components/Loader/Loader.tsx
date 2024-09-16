0497428009f293f079030778238b61c54ce4fe88
anastshak <nastyashakura@gmail.com>
2024-09-16 21:30:23 +0300

    feat: add loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import type { JSX } from 'react';
+import styles from './Loader.module.scss';
+
+export default function Loader(): JSX.Element {
+  return (
+    <div className={styles.loader} data-testid="loader">
+      <div className={styles.loaderSpinner}></div>
+    </div>
+  );
+}
