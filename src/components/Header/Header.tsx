9d1239c544bcf99060fc97d999be78a86aa77365
anastshak <nastyashakura@gmail.com>
2024-09-17 18:27:30 +0300

    feat: implement burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,22 @@ import museumLogo from '@/assets/museum-logo-dark-bg.svg';
 import bookmark from '@/assets/bookmark.svg';
 import home from '@/assets/home.svg';
 import { NavLink, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import classnames from 'classnames';
 
 import styles from './Header.module.scss';
 
 export const Header = () => {
   const { pathname } = useLocation();
+  const [isOpen, setOpen] = useState<boolean>();
 
   return (
     <header className={styles.header}>
-      <div>
+      <div className={styles.logo}>
         <img src={museumLogo} alt="museum logo" />
       </div>
 
-      <nav className={styles.navigation}>
+      <nav className={classnames(styles.navigation, isOpen ? styles.active : '')}>
         {pathname !== '/' ? (
           <NavLink to="/" className={styles.link}>
             <img src={home} alt="home icon" />
@@ -27,6 +30,11 @@ export const Header = () => {
           <span>Your favorites</span>
         </NavLink>
       </nav>
+
+      <button onClick={() => setOpen(!isOpen)} className={classnames(styles.btn, isOpen ? styles.active : '')}>
+        <div className={styles.burger1} />
+        <div className={styles.burger2} />
+      </button>
     </header>
   );
 };
